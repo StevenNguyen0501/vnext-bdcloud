bf4a107697e6432da1293580fa6944f29a290523
quocrainbi <quocrainbi@gmail.com>
2024-03-20 13:43:28 +0700

    example from flutter
    example from flutter

--- android/app/src/main/kotlin/com/example/camera_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.camera_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ example/android/app/src/main/kotlin/com/linecorp/linesdk/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.linecorp.linesdk.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
